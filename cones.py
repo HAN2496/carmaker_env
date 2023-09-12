fef22d5fc484e3aa2df3cc0d66ab1abbefe56968
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-09-12 18:55:28 +0900

    computer

--- cones.py
+++ cones.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from shapely.geometry import LineString
+from shapely.geometry import LineString, Point
 
 
 class Cone:
@@ -9,21 +9,13 @@ class Cone:
         self.type = type
         if type == 1:
             self.cones = self.create_DLC_cone()
+        elif type == 2:
+            self.cones = self.create_SLALOM_cone()
         else:
             self.cones = self.create_DLC_cone()
 #        self.cones_rel = self.to_relative_coordinates()
 
-    def create_DLC_cone(self):
-        sections = [
-            {'start': 0, 'gap': 5, 'cone_dist': 1.9748, 'num': 10, 'y_offset': -8.0525},
-            {'start': 50, 'gap': 3, 'cone_dist': 1.9748, 'num': 5, 'y_offset': -8.0525}, #
-            {'start': 62, 'gap': 2.7, 'cone_dist': 5.4684, 'num': 5, 'y_offset': -6.3057},
-            {'start': 75.5, 'gap': 2.75, 'cone_dist': 2.52, 'num': 5, 'y_offset': -4.8315}, #
-            {'start': 86.5, 'gap': 2.5, 'cone_dist': 5.981, 'num': 5, 'y_offset': -6.562},
-            {'start': 99, 'gap': 3, 'cone_dist': 3, 'num': 5, 'y_offset': -8.0525}, #
-            {'start': 111, 'gap': 5, 'cone_dist': 3, 'num': 15, 'y_offset': -8.0525}
-        ]
-
+    def create_cone(self, sections):
         cones = []
         for section in sections:
             for i in range(section['num']):  # Each section has 5 pairs
@@ -32,7 +24,34 @@ class Cone:
                 y2 = section['y_offset'] + section['cone_dist'] / 2
                 cones.extend([[x_base, y1], [x_base, y2]])
 
+
         return np.array(cones)
+
+    def create_DLC_cone(self):
+        sections = [
+            {'start': 0, 'gap': 5, 'cone_dist': 1.9748, 'num': 10, 'y_offset': -8.0525},
+            {'start': 50, 'gap': 3, 'cone_dist': 1.9748, 'num': 5, 'y_offset': -8.0525}, #
+            {'start': 64.7, 'gap': 2.7, 'cone_dist': 5.4684, 'num': 4, 'y_offset': -6.3057},
+            {'start': 75.5, 'gap': 2.75, 'cone_dist': 2.52, 'num': 5, 'y_offset': -4.8315}, #
+            {'start': 86.5, 'gap': 2.5, 'cone_dist': 5.981, 'num': 5, 'y_offset': -6.562},
+            {'start': 99, 'gap': 3, 'cone_dist': 3, 'num': 5, 'y_offset': -8.0525}, #
+            {'start': 111, 'gap': 5, 'cone_dist': 3, 'num': 15, 'y_offset': -8.0525}
+        ]
+
+        return self.create_cone(sections)
+
+    def create_SLALOM_cone(self):
+        sections = [
+            {'start': 0, 'gap': 5, 'cone_dist': 2, 'num': 18, 'y_offset': -5.25}, #
+            {'start': 100, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -2.625}, #
+            {'start': 110, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -5.25},
+            {'start': 120, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -5.25},
+            {'start': 130, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -7.875}, #
+            {'start': 140, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -5.25},
+            {'start': 150, 'gap': 60, 'cone_dist': 5.25, 'num': 5, 'y_offset': -5.25},
+            {'start': 400, 'gap': 10, 'cone_dist': 5.25, 'num': 15, 'y_offset': -5.25}
+        ]
+        return self.create_cone(sections)
 
     def cone_in_sight(self, carx, sight):
         return np.array([cone for cone in self.cones if carx - 2.1976004311961135 <= cone[0]][:sight*2])
@@ -54,6 +73,8 @@ class Cone:
     def pass_cone_line(self, cones_rel):
         width = 1.568
         l1, l2 = 2.1976004311961135, 4.3 - 2.1976004311961135
+        dist = 0
+        check = 0
 
         car_upper_line = LineString([(-l1, width/2), (l2, width/2)])
         car_lower_line = LineString([(-l1, -width/2), (l2, -width/2)])
@@ -62,10 +83,41 @@ class Cone:
             cone1 = cones_rel[i]
             cone2 = cones_rel[i+1]
             line = LineString([cone1, cone2])
-            if car_upper_line.intersects(line) and car_lower_line.intersects(line):
-                return 0
-        return 1
+            line_center = [(cone1[0] + cone2[0]) / 2, (cone1[1] + cone2[1]) / 2]
+           #자동차 영역에 있는지 확인
+            if car_upper_line.intersects(line) or car_lower_line.intersects(line):
+                check = 1
+                # 연결선 중 하나라도 모두 교차하지 않을 경우
+                if not (car_upper_line.intersects(line) and car_lower_line.intersects(line)):
+                    return 1
+        if check == 0:
+            return 1
+        else:
+            return 0
 
+    def distance_from_car_to_line(self, cones_rel, carx, cary):
+        width = 1.568
+        l1, l2 = 2.1976004311961135, 4.3 - 2.1976004311961135
+        closest_distance = float("inf")
+
+        car_upper_line = LineString([(-l1, width/2), (l2, width/2)])
+        car_lower_line = LineString([(-l1, -width/2), (l2, -width/2)])
+
+        for i in range(0, len(cones_rel) - 1, 2):
+            cone1 = cones_rel[i]
+            cone2 = cones_rel[i+1]
+            line = LineString([cone1, cone2])
+
+            # 연결선 중 자동차를 통과하는 연결선을 찾음
+            if car_upper_line.intersects(line) and car_lower_line.intersects(line):
+                # 해당 연결선의 중심점을 찾음
+                center = line.centroid
+                distance = center.distance(Point(carx, cary))
+
+                if distance < closest_distance:
+                    closest_distance = distance
+
+        return closest_distance
     def check_collision(self, cones_rel):
         width = 1.568
         length = 4.3
@@ -159,10 +211,8 @@ class Cone:
 """
 
 if __name__ == "__main__":
-    cones = Cone(1)
-    plt.scatter(cones.cones[:, 0], cones.cones[:,1])
-    plt.axis("equal")
-    carx, cary, caryaw = 60, -8, 0.1
+    cones = Cone(2)
+    carx, cary, caryaw = 60, -5.2, 0
     cone_sight = cones.cone_in_sight(carx, 5)
     print(cone_sight)
     cones_rel = cones.to_relative_coordinates(carx, cary, caryaw, cones.cones)
