bdfd385d6968412db59691c5cdf1dc6c00e1c240
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-09-18 15:27:56 +0900

    laptop

--- test _me.py
+++ test _me.py
@@ -1,2 +1,5 @@
-cones = [i for i in range(100, 400, 30)]
-print(cones)
+import numpy as np
+first_cone_arr = [[i, -5.25] for i in range(100, 400, 30)]
+second_cone_arr = [[i, -5.25] for i in range(600, 800, 30)]
+cone_arr = np.array(first_cone_arr + second_cone_arr)
+print(cone_arr)
