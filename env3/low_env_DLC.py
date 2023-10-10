3dbada893b0f6539116443ab4e722b275dc4bbb4
codemoo <hwanmooy@gmail.com>
2023-10-10 15:29:06 +0900

    y

--- env3/low_env_DLC.py
+++ env3/low_env_DLC.py
@@ -36,7 +36,7 @@ def cm_thread(host, port, action_queue, state_queue, action_num, state_num, stat
             time.sleep(1)
 
 class CarMakerEnv(gym.Env):
-    def __init__(self, host='127.0.0.1', port=10001, check=2, matlab_path='C:/CM_Projects/JX1_102/src_cm4sl', simul_path='pythonCtrl_DLC2', use_carmaker=True):
+    def __init__(self, host='127.0.0.1', port=10001, check=2, matlab_path='C:/CM_Projects/JX1_102/src_cm4sl', simul_path='pythonCtrl_DLC', use_carmaker=True):
         # Action과 State의 크기 및 형태를 정의.
         self.check = check
         self.road_type = "DLC"
