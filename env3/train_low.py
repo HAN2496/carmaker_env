515ca6137a2c0ce36f91246b999e6b1dedb384ca
codemoo <hwanmooy@gmail.com>
2023-10-11 22:26:08 +0900

    y

--- env3/train_low.py
+++ env3/train_low.py
@@ -8,7 +8,7 @@
 4. 학습이 완료된 후 웨이트 파일(e.g. model.pkl)을 저장한다.
 """
 import sys
-from low_env_DLC2 import CarMakerEnv
+from env_DLC_low2 import CarMakerEnv
 from stable_baselines3 import SAC, PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from callbacks import getBestRewardCallback, logDir, rmsLogging
