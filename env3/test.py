b377593ec2b21c271e277c6572ac9779a17e6001
codemoo <hwanmooy@gmail.com>
2023-10-12 17:16:48 +0900

    y

--- env3/test.py
+++ env3/test.py
@@ -5,7 +5,7 @@
 3. 테스트를 수행한다.
 """
 
-from low_env_DLC import CarMakerEnv
+from DLC_env_b import CarMakerEnvB
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,13 +14,14 @@ from stable_baselines3 import SAC
 if __name__ == '__main__':
     road_type = "DLC"
     env_num = "1"
-    data_name = 'IPG'
-    comment = ""
+    data_name = 'RL'
+    comment = "test"
     prefix = data_name + comment
 
 
-    env = CarMakerEnv(host='127.0.0.1', simul_path='test_IPG', port=9999)
-    model = SAC.load(f"model_forcheck/{road_type}/env1_2nd_last.pkl", env=env)
+    env = CarMakerEnvB(host='127.0.0.1', port=9999)
+#    model = SAC.load(f"models/{road_type}/491899_best_model.pkl", env=env)
+    model = SAC.load(f"models/DLC/env1_b/491899_best_model.pkl", env=env)
     print("Model loaded.")
 
     obs = env.reset()
