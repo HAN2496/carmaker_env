78e0342814f7ca8084b968a111406da2ca35d12c
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-10-15 18:17:36 +0900

    c

--- env3/test_me.py
+++ env3/test_me.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from stable_baselines3 import SAC
 
-print(np.zeros(4))
+# 모델 로드하기 (Python 3.8 이상 필요)
+model = SAC.load("best_model/SLALOM_env1_best_model.pkl")
+# 호환 가능한 프로토콜 사용하여 모델 다시 저장하기
+model.save("best_model/SLALOM_env1_best_model.pkl", exclude=["_vec_normalize_env"])
