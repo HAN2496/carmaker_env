09957148abd7adc39108da8a4a4f592583290840
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-10-17 17:10:33 +0900

    c

--- env3/SLALOM_data2.py
+++ env3/SLALOM_data2.py
@@ -36,7 +36,7 @@ class Data:
 
         self.traj_point = np.array([self.carx + self.point_interval * (self.point_num - 1), self.cary])
         self.traj_point_before = self.traj_point
-        self.traj_points = self.find_traj_points(self.carx)
+        self.traj_points = self.find_traj_points()
 
         if self.check == 0:
             self.render()
@@ -51,9 +51,9 @@ class Data:
         self.alHori, self.roll = arr[11:13]
 
     def make_traj_point(self, action):
-        theta = action * 0.1
-        new_traj_point = np.array([self.carx + 8 * np.cos(self.caryaw + theta),
-                                   self.cary + 8 * np.sin(self.caryaw + theta)])
+        theta = action * 0
+        new_traj_point = np.array([self.carx + 8 * np.cos(theta),
+                                   self.cary + 8 * np.sin(theta)])
         return new_traj_point
 
     def make_trajectory(self, action):
@@ -72,14 +72,27 @@ class Data:
         else:
             return arr
 
-    def find_traj_points(self, x0):
-        points = []
-        for i in range(self.point_num):
-            distance = i * self.point_interval
-            x_diff = np.abs(self.traj_data[:, 0] - (x0 + distance))
-            nearest_idx = np.argmin(x_diff)
-            points.append(self.traj_data[nearest_idx])
-        return np.array(points)
+    def find_traj_points(self):
+        distances = np.array([self.point_interval * i for i in range(self.point_num - 1)])
+        result_points = []
+
+        min_idx = np.argmin(np.sum((self.traj_data - np.array([self.carx, self.cary])) ** 2, axis=1))
+
+        for dist in distances:
+            lookahead_idx = min_idx
+            total_distance = 0.0
+            while total_distance < dist and lookahead_idx + 1 < len(self.traj_data):
+                total_distance += np.linalg.norm(self.traj_data[lookahead_idx + 1] - self.traj_data[lookahead_idx])
+                lookahead_idx += 1
+
+            if lookahead_idx < len(self.traj_data):
+                result_points.append(self.traj_data[lookahead_idx])
+            else:
+                result_points.append(self.traj_data[-1])
+
+        result_points.append(self.traj_point)
+
+        return result_points
 
     def calculate_dev(self):
         arr = np.array(self.traj_data)
@@ -122,14 +135,11 @@ class Data:
         self.traj_data = self.make_trajectory(blevel_action)
 
         #새로생긴 point가 데이터 중간에 생성되었을수도 있으므로. 뺑뺑돌면 가능은 하겠다.
-        self.traj_points = self.find_traj_points(self.carx)
+        self.traj_points = self.find_traj_points()
         traj_rel = self.to_relative_coordinates(self.traj_points).flatten()
 
         cones_abs = self.cone.cones_arr[self.cone.cones_arr[:, 0] > self.carx][:3]
         cones_rel = self.to_relative_coordinates(cones_abs).flatten()
-
-        middle_abs = self.cone.middles_arr[self.cone.middles_arr[:, 0] > self.carx][:2]
-        middle_rel = self.to_relative_coordinates(middle_abs).flatten()
 
         state = np.concatenate((np.array([self.carx, self.cary, self.caryaw]), traj_point_new_rel, cones_rel)) # <- Policy B의 state
         reward_argument = {"traj": traj_point_new, "caryaw": self.caryaw}
--- env3/test_me.py
+++ env3/test_me.py
@@ -2,8 +2,6 @@ from stable_baselines3 import SAC
 import pickle
 
 # 모델 불러오기
-model = SAC.load("best_model/SLALOM_env1_best_model.pkl")
+a = 10
 
-# 호환 가능한 프로토콜로 모델 다시 저장
-with open("best_model/SLALOM_env1_best_model_compatible.pkl", "wb") as file:
-    pickle.dump(model, file, protocol=4)
+print(a%7)
