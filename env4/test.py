c4280e5be6168ef4557d83a1107ccd0bb8fa1924
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-10-23 15:48:36 +0900

    1

--- env4/test.py
+++ env4/test.py
@@ -12,11 +12,10 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import SAC
 
 if __name__ == '__main__':
-    road_type = "SLALOM"
+    road_type = "DLC"
     data_name = 'IPG'
-    comment = "low"
+    comment = "cutting0"
     prefix = data_name + "_" + comment
-
 
     env = CarMakerEnv(check=0, simul_path='test_IPG', road_type=road_type)
     model = SAC.load(f"best_model/model.pkl", env=env)
