dfeaddc63b22710a02d6da5434443ab6eae80c39
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-11-03 17:53:14 +0900

    Update DLC_env_low3.py

--- env4/DLC_env_low3.py
+++ env4/DLC_env_low3.py
@@ -182,7 +182,7 @@ class CarMakerEnv(gym.Env):
         #devDist, devAng에 따른 리워드
         reward_devDist = dev_dist * 1000
         if self.road_type == 'DLC':
-            reward_devAng = dev_ang * 1000
+            reward_devAng = dev_ang * 5000
         else:
             reward_devAng = dev_ang * 5000
 
