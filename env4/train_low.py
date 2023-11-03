7f7a9173647b7ab9463e7d93eb6c5d72ce9c7b76
codemoo <hwanmooy@gmail.com>
2023-11-03 17:35:18 +0900

    2

--- env4/train_low.py
+++ env4/train_low.py
@@ -8,7 +8,7 @@
 4. 학습이 완료된 후 웨이트 파일(e.g. model.pkl)을 저장한다.
 """
 import sys
-from DLC_env_low2 import CarMakerEnv
+from DLC_env_low3 import CarMakerEnv
 from stable_baselines3 import SAC, PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from callbacks import getBestRewardCallback, logDir, rmsLogging
@@ -64,10 +64,10 @@ def main():
     4. 추가 설명 내용이 있을 경우 explanation에 글을 작성하면 Log.txt에 기록됨
     """
 
-    env_num = 2
+    env_num = 3
     road_type = "DLC"
     comment = "low"
-    explanation = "State changed. Cone arr add"
+    explanation = "cone data add in reward and state"
 
     num_proc = 2
     naming = f"env{env_num}_{comment}"
