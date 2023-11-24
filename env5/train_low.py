0f4a837aadc5f3cd46f01e22b0ead7cc971f30fd
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-11-24 17:41:45 +0900

    Update train_low.py

--- env5/train_low.py
+++ env5/train_low.py
@@ -65,11 +65,11 @@ def main():
     """
 
     env_num = 1
-    road_type = "SLALOM2"
-    comment = "test"
+    road_type = "DLC"
+    comment = "rws"
     explanation = "carmaker env low"
 
-    num_proc = 2
+    num_proc = 1
     naming = f"env{env_num}_{comment}"
     prefix = road_type + "/" + naming
     args = Args(prefix=prefix, alg='sac')
