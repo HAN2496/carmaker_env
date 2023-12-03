702dac53afa4e5f1039fc74deeb9be6fd0f71e77
codemoo <hwanmooy@gmail.com>
2023-12-04 03:36:47 +0900

    2

--- env6/train_low.py
+++ env6/train_low.py
@@ -64,12 +64,12 @@ def main():
     4. 추가 설명 내용이 있을 경우 explanation에 글을 작성하면 Log.txt에 기록됨
     """
 
-    road_type = "SLALOM2"
-    comment = "d"
+    road_type = "DLC"
+    comment = "rws"
     explanation = "carmaker env low"
 
-    num_proc = 2
-    naming = f"env_{comment}"
+    num_proc = 5
+    naming = f"{comment}_low"
     prefix = road_type + "/" + naming
     args = Args(prefix=prefix, alg='sac')
 
